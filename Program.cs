ba407447fad038f48fad8314c6c5a4a7dc086357
rafaelpin <skynet.repo@gmail.com>
2022-05-13 13:01:44 -0300

    crianção class estudante e inicio do pogram principal

--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace Exercicio_Vetores
     {
         static void Main(string[] args)
         {
+
+            Estudante[] vect = new Estudante[10];
+
+            Console.WriteLine("Quantos quartos serão alugados?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+
+
+
+            }
+        
+        
+        
+        
+        
+        
+        
+        
+        
         }
     }
 }
